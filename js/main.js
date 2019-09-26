a3a272027a69fa558b74bbe47e6761279d9ba045
garretlisto <54542130+garretlisto@users.noreply.github.com>
2019-09-26 11:07:48 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function myfunction() {
+    var x = document.getElementById("firstName").value;
+    var y = document.getElementById("lastName").value;
+    document.getElementById("demo").innerHTML= "Hello " + x +" "+ y;
+    
+    }
